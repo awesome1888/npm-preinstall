8a58c64553ca9478bd877088eb0d11581858bb7d
Sergei <gannochenko.sv@gmail.com>
2019-01-01 00:52:44 +0100

    done

---
+++ src/execute.js
@@ -0,0 +1,35 @@
+import { spawn } from 'child_process';
+
+const execute = (cmd, args = [], params = {}) => {
+    args = args || [];
+
+    const stdoutTo = params.stdoutTo || process.stdout;
+    const stderrTo = params.stderrTo || process.stderr;
+
+    return new Promise((resolve) => {
+        const handle = spawn(cmd, args, {
+            cwd: process.cwd(),
+            stdio: 'inherit',
+            shell: true,
+        });
+
+        if (stdoutTo && handle.stdout) {
+            handle.stdout.pipe(stdoutTo);
+        }
+        if (stderrTo && handle.stderr) {
+            handle.stderr.pipe(stderrTo);
+        }
+
+        handle.on('error', () => {
+            resolve(1);
+        });
+        handle.on('close', (code) => {
+            resolve(code);
+        });
+        handle.on('exit', (code) => {
+            resolve(code);
+        });
+    });
+};
+
+export default execute;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-// import 'babel-polyfill';
+import '@babel/polyfill';
+import fs from 'fs';
+import execute from './execute';
+import { areFilesEqual } from 'fs-equal';
+import copyFilesSync from 'fs-copy-file-sync';
 
-// write code here ...
+(async () => {
+
+    const file = `${process.cwd()}/package.json`;
+    const fileLock = `${process.cwd()}/.packagejson.lock`;
+
+    if (fs.existsSync(file)) {
+        let equal = false;
+        try {
+            equal = await areFilesEqual(file, fileLock);
+        } catch (e) {
+        }
+
+        if (!equal) {
+            await execute('npm', ['install']);
+            copyFilesSync(file, fileLock);
+        }
+    }
+
+    process.argv.shift(); // drop the command itself
+    process.argv.shift(); // drop the command itself
+    const cmdToRun = process.argv.shift();
+
+    if (cmdToRun && cmdToRun.length) {
+        await execute(cmdToRun, process.argv);
+    }
+})();
